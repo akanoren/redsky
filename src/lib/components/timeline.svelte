774707832b6a597614f0b188461ee4ac5eade377
Minori Tokuda <snowcrush@mail.0pt.jp>
2023-03-06 00:41:31 +0900

    修正漏れのstyle in componentを削除

--- src/lib/components/timeline.svelte
+++ src/lib/components/timeline.svelte
@@ -4,31 +4,6 @@
   import Post from "$lib/components/post.svelte";
 </script>
 
-<style>
-  .refresh-button {
-    width: 8em;
-    border-color: #6A5ACD;
-    background-color: #6A5ACD;
-    border-radius: 20px;
-    padding:5px 15px 5px 15px;
-    color: #FFFFFF;
-  }
-
-  .to-right {
-    width: 100%;
-    display: flex;
-    justify-content: end;
-  }
-
-  .timeline {
-    margin-top: 1.5em;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    row-gap: 0.5em;
-  }
-</style>
-
 <div class="to-right">
   <button class="refresh-button" on:click={refresh}>Refresh</button>
 </div>
