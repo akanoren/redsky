5a2fd9b9b2b39fc356e5ce961ccd43bbd9cca3c0
Dongri Jin <dongrify@gmail.com>
2023-03-06 22:54:03 +0900

    Add timestamp

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import type { AtpAgent } from "@atproto/api";
-
+  import TimeAgo from 'javascript-time-ago';
+  import en from 'javascript-time-ago/locale/en'
+  TimeAgo.addDefaultLocale(en)
+  
   type Post = Awaited<ReturnType<InstanceType<typeof AtpAgent>['api']['app']['bsky']['feed']['getTimeline']>>['data']['feed'][number];
 
   export let post: Post;
+
+  const timeAgo = new TimeAgo('en-US');
 
   function getText(post: Post) {
     type Record = Post['post']['record'];
@@ -20,7 +25,12 @@
   <div class="avatar">
     <img src={post.post.author.avatar} alt=""/>
   </div>
-  <div class="name">{post.post.author.displayName}</div>
+  <div class="name">
+    {post.post.author.displayName}
+    <span class="time">
+      {timeAgo.format(new Date(post.post.indexedAt))}
+    </span>
+  </div>
   <div class="message">
     <div >{getText(post)}</div>
   </div>
